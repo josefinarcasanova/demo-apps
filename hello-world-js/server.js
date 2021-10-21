b4cc0925c5e80311c4b04fef4d309f53ee353204
Josefina R. Casanova <josefinarcasanova@ibm.com>
2021-10-21 12:23:55 -0300

    Hello World

--- hello-world-js/server.js
+++ hello-world-js/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer(function (request, response) {
-   target = process.env.TARGET ? process.env.TARGET : 'Josefina!' ;
+   target = process.env.TARGET ? process.env.TARGET : 'Worold!' ;
    msg = process.env.MSG ? process.env.MSG : 'Hello ' + target + '\n';
    response.writeHead(200, {'Content-Type': 'text/plain'});
    response.end(msg);
