e4a2327815c941d7a81f7a423dd83a69ba1fc0ed
Josefina R. Casanova <josefinarcasanova@ibm.com>
2022-04-19 10:29:22 -0300

    Added redundancies

--- current-time-insert-py/src/database.py
+++ current-time-insert-py/src/database.py
@@ -1,31 +1,36 @@
 import pymongo
 
 from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
 from icos import create_cos_client, get_cos_file
 
 from os import environ
 from dotenv import load_dotenv
 
 # Load environment variables from .env
-load_dotenv()   
+load_dotenv()
 
 def connect_to_database():
-    # Connect to mongodb using pymongo
-    mongo_connect_str = environ['MONGO_URL']
+    try:
+        # Connect to mongodb using pymongo
+        mongo_connect_str = environ['MONGO_URL']
 
-    tls_file = retreive_certificate()
-    tls_file_name = environ['TLS_FILE_NAME']
+        tls_file = retreive_certificate()
+        tls_file_name = environ['TLS_FILE_NAME']
 
-    # Create a connection using MongoClient
-    client = MongoClient(
-        mongo_connect_str,
-        tls=True,
-        tlsCAFile=tls_file_name
-    )
+        # Create a connection using MongoClient
+        client = MongoClient(
+            mongo_connect_str,
+            tls=True,
+            tlsCAFile=tls_file_name
+        )
 
-    database = client[environ['MONGO_DB_NAME']]
+        database = client[environ['MONGO_DB_NAME']]
 
-    return database
+        return database
+    
+    except ConnectionFailure as err:
+        print(err)        
 
 def retreive_certificate():
     return get_cos_file(
@@ -63,11 +68,17 @@ def insert_data(database, json_object):
     collection_name = environ['MONGO_COLLECTION_NAME']
     collection      = database[collection_name]
 
+    # Get collection size, pre insertion
+    pre_count = collection.count_documents({})
+
     # Insert new item
     insert_result = collection.insert_one(json_object)
+
+    # Get collection size, post insertion
+    post_count = collection.count_documents({})
         
     # Close database connection
     database.client.close()
 
     #return insert_result
-    return insert_result.inserted_id != None
+    return (pre_count + 1) == (post_count)
